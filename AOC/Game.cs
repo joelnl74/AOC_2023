64988b57a2fba8ed108f224ea6697f9e0f0c9d42
Joey <joelnl74@gmail.com>
2023-12-03 15:13:53 +0100

    AOC 2

---
+++ AOC/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace AOC
+{
+    public struct Game
+    {
+        const int VALID_RED = 12;
+        const int VALID_GREEN = 13;
+        const int VALID_BLUE = 14;
+
+        public bool IsValid { get; private set;  }
+        public int Id { get; private set; }
+
+        readonly int red;
+        readonly int green;
+        readonly int blue;
+
+        public Game(int id, int r, int g, int b)
+        {
+            Id = id;
+            red = r; 
+            green = g; 
+            blue= b;
+
+            IsValid = red <= VALID_RED && blue <= VALID_BLUE && green <= VALID_GREEN;
+        }
+    }
+}
--- AOC/Program.cs
+++ AOC/Program.cs
@@ -1,4 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // string line in File.ReadLines("../../../Resources/aoc.txt"
 
-Console.WriteLine("Hello world");
+using AOC;
+
+List<Game> gamesPlayed = new List<Game>();
+
+Main();
+
+int Main()
+{
+    int index = 1;
+    int green = 0;
+    int blue = 0;
+    int red = 0;
+    string value = "";
+    bool checkingNumber = false;
+
+    foreach (string line in File.ReadLines("../../../Resources/aoc.txt"))
+    {
+        red = 0;
+        blue = 0;
+        green = 0;
+        
+        for (int i = 8; i < line.Length; i++)
+        {
+            var isNumber = Char.IsNumber(line[i]);
+        
+            if (isNumber)
+            {
+                checkingNumber = true;
+                value += line[i];
+            }
+
+            if (checkingNumber == true && isNumber == false)
+            {
+                checkingNumber = false;
+                
+                // Indicates the color with r g b.
+                var charachterColor = line[i + 1];
+                Update(charachterColor, ref red, ref green, ref blue, int.Parse(value.ToString()));
+                value = "";
+            }
+        }
+
+        gamesPlayed.Add(new Game(index, red, green, blue));
+        index++;
+    }
+
+    var validGames = gamesPlayed.Where(x => x.IsValid).ToArray();
+    Console.WriteLine(validGames.Sum(y => y.Id));
+
+    return 1;
+}
+
+void Update(char color, ref int r, ref int g, ref int b, int value)
+{
+    switch (color)
+    {
+        case 'r':
+            r = value > r ? value : r;
+            break;
+        case 'g':
+            g = value > g ? value : g;
+            break;
+        case 'b':
+            b = value > b ? value : b;
+            break;
+    }
+}
